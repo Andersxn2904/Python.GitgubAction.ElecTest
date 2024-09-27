945882baeb178f5955730e641e786a63c9fb4b3d
Anderson Figuereo Lorenzo <142941034+Andersxn2904@users.noreply.github.com>
2024-09-27 13:24:11 -0400

    new commit

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 def add(a, b):
     return a + b
 
+def Sustract(a, b):
+    return a - b
+
 if __name__ == "__main__":
     print(add(2, 3))
 
--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 # test_app.py
 import unittest
-from app import add
+from app import add, Sustract
 
 class TestApp(unittest.TestCase):
 
@@ -8,6 +8,10 @@ class TestApp(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+        
+    def test_Sustract(self):
+        self.assertEqual(Sustract(5, 2), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
